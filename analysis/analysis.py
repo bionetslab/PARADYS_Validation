b153147ba8547c0a1e446dafd9c1306121416a76
fabiwoller <fabianwoller@web.de>
2024-03-22 11:14:26 +0100

    Clean up of analysis script and add README of analysis workflow.

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,47 +1,13 @@
 import json
 import numpy as np
 import pandas as pd
-from scipy import stats
-from os.path import exists
-import multiprocessing
-from multiprocessing import Pool
-import itertools
 import os
 from scipy.stats import chisquare, chi2
 import networkx as nx
 
-cancer = 'PRAD_Decoy_075' 
-kappa = 3
-scores = False
-
-os.chdir("../")
-
-
-
-print("Loading preprocessed data...")
-
-with open('filtered_data/'+cancer+'/patients.json') as i:
-    patients = json.load(i)
-
-print("Number of patients:", len(patients))
-
-with open('filtered_data/'+cancer+'/genes_M.json') as i:
-    genes_M = json.load(i)
-
-with open('filtered_data/'+cancer+'/dic_patients_edges.json') as i:
-    dic_patients_edges = json.load(i)
-
-with open('filtered_data/'+cancer+'/dic_mutations_patients.json') as i:
-    dic_mutations_patients = json.load(i)
-
-with open('filtered_data/'+cancer+'/dic_patients_mutations.json') as i:
-    dic_patients_mutations = json.load(i)   
-    
-
-
 def calculate_page_rank(data, d=0.85, directed=True):
     '''
-    Creates a linegraph and applies pagerank algorithm to get driver scores
+    Creates a linegraph and applies pagerank algorithm to get driver scores.
 
     Parameters
     ----------
@@ -55,11 +21,10 @@ def calculate_page_rank(data, d=0.85, directed=True):
     Returns
     -------
     data : DataFrame
-        
-    Same as input, with additional column with scores and ordered in descending score
+        Same as input, with additional column with scores and ordered in descending score.
 
     '''
-    # Extract unique drivers and edges
+    # Extract unique drivers and edges.
     total_drivers = set(data['drivers'])
     target_edges = set(data['dysregulations'])
     
@@ -85,10 +50,10 @@ def calculate_page_rank(data, d=0.85, directed=True):
 
     nx.set_node_attributes(G, vertex_weights, 'weight')
 
-    # Preprocess edges to remove unwanted characters and create a dictionary for quick access
+    # Preprocess edges to remove unwanted characters and create a dictionary for quick access.
     processed_edges = {edge.replace("(", "").replace(")", "").replace("'", "").replace(" ", ""): edge for edge in target_edges}
     
-    # Add edges between connected edges with weight 1
+    # Add edges between connected edges with weight 1.
     for edge in target_edges:
         edge1 = edge.partition(', ')[2].replace("(", "").replace(")", "").replace("'", "").replace(" ", "")
         for edge2 in target_edges:
@@ -96,18 +61,15 @@ def calculate_page_rank(data, d=0.85, directed=True):
             if edge0 == edge1:
                 G.add_edge(edge, processed_edges[edge2], weight=1)
     
-    
-
-
-    # Add edges between genes and edges with weight -log(p-value)
+    # Add edges between genes and edges with weight -log(p-value).
     for driver in total_drivers:
         data_driver = data[data['drivers'] == driver]
         for edge in data_driver['dysregulations']:
             G.add_edge(edge, driver, weight=-np.log(data_driver[data_driver['dysregulations'] == edge]['p-values'].values[0]))
 
-    # Apply PageRank algorithm and store scores
+    # Apply PageRank algorithm and store scores.
     pr = nx.pagerank(G, alpha=d, personalization=vertex_weights, weight='weight')
-    # Add scores as a new column in the original data DataFrame
+    # Add scores as a new column in the original data DataFrame.
     data['PageRank_Score'] = data['drivers'].map(pr)
 
     # Sort the DataFrame based on the scores
@@ -116,45 +78,51 @@ def calculate_page_rank(data, d=0.85, directed=True):
     return data
 
 
- 
-
-def get_relations(kappa, cancer, patients, dic_patients_mutations):
+def get_relations(kappa, cancer, patients, dic_patients_mutations, 
+                  dic_patients_graphs, output_dir, save_file=False):
     '''
-    Returns and stores all connections between mutations and dysregulations
+    Computes and stores all connections between mutations and dysregulations.
 
     Parameters
     ----------
     kappa : int
-        Maximum path length between edge and mutated gene
+        Maximum shortest path length between edge and mutated gene.
     cancer : str
-        Cancer type
+        Cancer type.
     patients : list
-        List of patient IDs
+        List of patient IDs.
     dic_patients_mutations : dict
-        Dictionary containing mutated genes for each patient
+        Dictionary containing mutated genes for each patient.
+    dic_patients_graphs: dict
+        Dictionary with keys as patients and values are dictionaries that have TFs as keys and
+        genes connected to them as values.
+    output_dir : str
+        Name of output directory for storing kappa neighborhood relation file.
+    save_file : bool
+        Whether or not to save the computed resulting neighborhood relation csv file (large!).
 
     Returns
     -------
     data : DataFrame
-        For every mutated gene for every patient, list of dysregulations connected to it in a max distance kappa
-        Columns are genes (mutations), patients (patient ids) and connected dysregulations (genes connected to the mutation)
+        For every mutated gene for every patient, list of dysregulations connected to it 
+        within a max shortest-path distance kappa.
+        Columns are genes ('genes'), patients ('patients') and 
+        genes connected to the mutation ('connected dysregulations').
 
     '''
-    with open('filtered_data/'+cancer+'/dic_patients_graphs.json') as i:
-        dic_patients_graphs = json.load(i)
-
-    gs=[] # mutations
-    ps=[] # patients
-    ds=[] # connected genes
+    found_mutations=[] 
+    found_patients=[] 
+    connected_genes=[] 
+    
     for p in patients:
         genes_p = dic_patients_mutations.get(p, [])
         edges = dic_patients_graphs.get(p, {})
-
+        # Process all mutated genes.
         for g in genes_p:
             if g in edges:
                 connected_dysregulations = set()
                 genes_stack = [g]
-
+                # Compute kappa neighborhood.
                 for _ in range(kappa):
                     next_genes = set()
 
@@ -167,23 +135,26 @@ def get_relations(kappa, cancer, patients, dic_patients_mutations):
 
                 if connected_dysregulations:
                     for dysregulation in connected_dysregulations:
-                        gs.append(g)
-                        ps.append(p)
-                        ds.append(dysregulation)
+                        found_mutations.append(g)
+                        found_patients.append(p)
+                        connected_genes.append(dysregulation)
+    
     data = pd.DataFrame({
-        'connected dysregulations': ds,
-        'genes': gs,
-        'patients': ps
+        'connected dysregulations': connected_genes,
+        'genes': found_mutations,
+        'patients': found_patients
     })
-    data.to_csv('filtered_data/'+cancer+'/gene_sets_kappa' + str(kappa) + '.csv', index=False)
+    
+    # Store neighborhood file if desired.
+    if save_file:
+        data.to_csv(output_dir+'gene_sets_kappa'+str(kappa)+'.csv', index=False)
+    
     return data
-
-
 
 
 def mutation_condition (gene,patient):
     '''
-    Mutation condition
+    Check if gene is mutated for given patient.
 
     Parameters
     ----------
@@ -208,11 +179,9 @@ def mutation_condition (gene,patient):
         return False
         
 
-
-
 def dysregulation_condition(edge, patient):
     '''
-    Dysregulation condition
+    Check if dysregulation edge is present in patient's network.
 
     Parameters
     ----------
@@ -222,7 +191,7 @@ def dysregulation_condition(edge, patient):
     Returns
     -------
     bool
-        True if edge is present in the patient's dysregulatory network, False otherwise
+        True if edge is present in the patient's dysregulatory network, False otherwise.
 
     '''
     if edge in dic_patients_edges[patient]:
@@ -233,20 +202,21 @@ def dysregulation_condition(edge, patient):
 
 def obs(p, edge_string, gene):
     '''
-    Create observation matrix for Chi2 test
+    Create per-patient observation matrix for Chi2 test.
 
     Parameters
     ----------
     p : string
-        Patient.
+        Patient ID.
     edge_string : string
-        Edge.
+        Edge in string format, e.g. (gene1, gene2).
     gene : string
-
+        Gene label.
+        
     Returns
     -------
     o : DataFrame
-        2x2 Observation matrix.
+        2x2 observation matrix.
 
     '''
     o = np.zeros((2, 2), dtype=int)
@@ -263,29 +233,33 @@ def obs(p, edge_string, gene):
     return o
 
 
-def get_o(edge_string, gene):
+def get_o(edge_string, gene, patients):
+    """Computes aggregated observation matrix over all patients.
+
+    Args:
+        edge_string (str): Edge in the string format, e.g. (gene1, gene2).
+        gene (str): Name of the mutated gene to analyse wrt the given edge.
+        patients (list): Patients in considered cohort.
+    Returns:
+        np.array: 2x2 observation matrix.
+    """
     o=np.array([[0,0],[0,0]])
     obs_results = np.array([obs(p, edge_string, gene) for p in patients])
     return np.sum(obs_results, axis=0)
 
-    # for result in map(obs, patients, itertools.repeat(edge_string, len(patients)), itertools.repeat(gene, len(patients))):
-    #     o=o+result
-    # return o
 
-
-def chi(edge, gene, edge_string):
+def chi(gene, edge_string, patients):
     '''
-    Calculate chi2 statistic for a given gene and edge
+    Calculate chi2 test statistic for given gene and edge.
 
     Parameters
     ----------
-    edge : list --> first value is source, second value is target
-        
-    gene : string --> name of the mutated gene to analyse wrt the given edge
-        
-    
-    edge_string : string --> edge in the string format --> (gene1,gene2)
-        
+    gene : string 
+        Name of the mutated gene to analyse wrt the given edge.
+    edge_string : string 
+        Edge in the string format, e.g. (gene1,gene2).
+    patients: list
+        List of all patients in the cohort.
 
     Returns
     -------
@@ -293,10 +267,12 @@ def chi(edge, gene, edge_string):
         Chi-square value for that gene and edge
 
     '''
-    o = list(get_o(edge_string, gene))
+    # Compute observation matrix.
+    o = list(get_o(edge_string, gene, patients))
     row_totals = np.array([np.sum(o, axis=1)])
     col_totals = np.array([np.sum(o, axis=0)])
     n = np.sum(o)
+    # Compute expectance matrix.
     e = np.dot(row_totals.T, col_totals) / n
     chisq, p = chisquare(o, e)
     chisq = np.sum(chisq)
@@ -304,70 +280,109 @@ def chi(edge, gene, edge_string):
     return p
 
 
-
-# only if not all edges are goint to be analysed:
-# print("Opening edge counts...")
-# with open('filtered_data/'+cancer+'/edges_counts.json') as h:
-#     edges_counts = json.load(h)
-
-
-
-
-print("Checking if gene sets data already exists...")
-
-if exists('filtered_data/'+cancer+'/gene_sets_kappa'+str(kappa)+'.csv'): 
-    gene_sets = pd.read_csv('filtered_data/'+cancer+'/gene_sets_kappa'+str(kappa)+'.csv')
-else:
-	gene_sets = get_relations(kappa,cancer, patients, dic_patients_mutations)
+if __name__ == "__main__":
     
+    os.chdir("../")
     
+    ### Input parameters.
     
-'''
-iterates through every patient and computes patient-specific results
-'''
-for p in patients:
-    print(p + ' calculating...')
-    
-    data = pd.DataFrame()  # Results dataframe that will be stored in the results folder
-    genes_sigs = []  # Drivers (chi2 p_val < 0.05)
-    chis_sig = []  # P_vals that were < 0.05 (p_vals of drivers)
-    edge_strings=[] # dysregulations associated to drivers
-    analysed_genes = set()  # To analyze every gene only once
-    gene_sets_p = gene_sets[gene_sets['patients'] == p] # acces for the given patient the mutations and genes connected to mutations
-    genes = set(gene_sets_p['connected dysregulations']) # get all mutations and genes connected to mutations, the rest of genes are not of interest for analysis
+    # Name of cancer cohort to be analyzed.
+    cancer = 'PRAD_Decoy_075' 
+    # Size of neighbor to consider for putative driver identification.
+    kappa = 3
+    # Whether to compute driver scores based on PageRank algorithm.
+    scores = False
+    # Input directory containing preprocessed files.
+    input_dir = f'filtered_data/{cancer}/'
+    # Output directory for storing result files.
+    output_dir = f'results/{cancer}/'
 
-    for edge in dic_patients_edges[p]:
-        edge_string = edge
-        edge = [e.strip("() '") for e in edge.split(', ')]
-        edge0, edge1 = edge
+    ### Loading of preprocessed data.
+    
+    print("Loading preprocessed data...")
+
+    with open(input_dir+'patients.json') as i:
+        patients = json.load(i)
+
+    with open(input_dir+'genes_M.json') as i:
+        genes_M = json.load(i)
+
+    with open(input_dir+'dic_patients_edges.json') as i:
+        dic_patients_edges = json.load(i)
+
+    with open(input_dir+'dic_mutations_patients.json') as i:
+        dic_mutations_patients = json.load(i)
+
+    with open(input_dir+'dic_patients_mutations.json') as i:
+        dic_patients_mutations = json.load(i)   
         
-        if edge0 in genes and edge0 not in analysed_genes: # make sure the edge is not analysed and the tf is mutated or connected to mutation, otherwise not of interest
-            analysed_genes.add(edge0)
-            target_genes_set = set(gene_sets_p[gene_sets_p['connected dysregulations'] == edge0]['genes']) # get candidate drivers (mutations that are connected to the given dysregulation)
-            visited = set()
-            for gene in target_genes_set: # iterate every candidate driver
-                key = str([edge, gene])
-                if key not in visited: # make sure the combination edge and gene is not already analysed
-                    visited.add(key)
-                    chi_g_e = chi(edge, gene, edge_string)
-                    if chi_g_e < 0.05: # if p_val < 0.05 then the mutation is considered a driver
-                        edge_strings.append(edge_string)
-                        chis_sig.append(chi_g_e)
-                        genes_sigs.append(gene)
-                   
-              
-            
-   
-    data = pd.DataFrame({'drivers': genes_sigs, 'p-values': chis_sig, 'dysregulations': edge_strings, 'patient': p})
-    print('Number of drivers:', len(genes_sigs))
-    if genes_sigs:
-        if scores:
-            df = calculate_page_rank(data, d=0.85)  # Concatenate all data at once
-            df.to_csv(f'results/{cancer}/{p}_scores.csv', index=False)
-        else:
-            data.to_csv(f'results/{cancer}/{p}.csv', index=False)
+    with open(input_dir+'dic_patients_graphs.json') as i:
+        dic_patients_graphs = json.load(i)
 
-    print(p + ' results computed')
+    # Checking if gene dysregulation neighborhood information has already been computed and stored.
+    # Speeds up computation if this information can simply be loaded.
+    gene_sets_file = 'gene_sets_kappa'+str(kappa)+'.csv'
+    if os.exists(input_dir+gene_sets_file): 
+        gene_sets = pd.read_csv(input_dir+gene_sets_file)
+    else:
+        save_file = True
+        gene_sets = get_relations(kappa,cancer, patients, dic_patients_mutations, 
+                                  dic_patients_graphs, input_dir, save_file)
 
+    
+    # Process all given patients.
+    for p in patients:
 
-print("FINISHED")
+        # Results dataframe that will be stored in the results folder.
+        data = pd.DataFrame()
+        # List of significant drivers.  
+        genes_sigs = []
+        # Pvalues of significant drivers.  
+        chis_sig = []  
+        # Dysregulations associated to drivers.
+        edge_strings=[] 
+        # To analyze every gene only once.
+        analysed_genes = set()  
+        
+        # For the given patient, access mutations and genes connected to mutations.
+        gene_sets_p = gene_sets[gene_sets['patients'] == p]
+        # Sample all mutations and genes connected to mutations, the remaining genes are not of interest for analysis.
+        genes = set(gene_sets_p['connected dysregulations'])
+
+        # Process all patient-specific edges.
+        for edge in dic_patients_edges[p]:
+            edge_string = edge
+            edge = [e.strip("() '") for e in edge.split(', ')]
+            edge0, edge1 = edge
+
+            # Only consider edges that are not analysed yet and whose tf is mutated or connected to mutation.
+            if edge0 in genes and edge0 not in analysed_genes: 
+                analysed_genes.add(edge0)
+                # Extract candidate drivers (i.e. mutations that are connected to the given dysregulation).
+                target_genes_set = set(gene_sets_p[gene_sets_p['connected dysregulations'] == edge0]['genes']) 
+                visited = set()
+                # Process every candidate driver.
+                for gene in target_genes_set: 
+                    key = str([edge, gene])
+                    # Make sure the combination of edge and gene is not already analysed.
+                    if key not in visited: 
+                        visited.add(key)
+                        chi_g_e = chi(gene, edge_string, patients)
+                        # Significance thresholding.
+                        if chi_g_e < 0.05: 
+                            edge_strings.append(edge_string)
+                            chis_sig.append(chi_g_e)
+                            genes_sigs.append(gene)
+
+        # Create driver results dataframe.
+        data = pd.DataFrame({'drivers': genes_sigs, 'p-values': chis_sig, 'dysregulations': edge_strings, 'patient': p})
+        
+        # If at least one driver has been detected, check if scoring is desired.
+        if genes_sigs:
+            if scores:
+                df = calculate_page_rank(data, d=0.85) 
+                df.to_csv(output_dir+f'{p}_scores.csv', index=False)
+            else:
+                data.to_csv(output_dir+f'{p}.csv', index=False)
+
+    print("FINISHED")
