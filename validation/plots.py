d6a00d281548e59dfd1b26f39ab98f09596d31b8
fabiwoller <fabianwoller@web.de>
2024-04-02 15:15:12 +0200

    Implement plotting functionality for FDRs and gleason/os/xcell plots.

--- validation/plots.py
+++ validation/plots.py
@@ -80,14 +80,14 @@ def plot_gleason_os_xcell(cluster_file_path : str, phenotype_file : str, surviva
         os = get_survival_times_of_cluster(cluster_file_path, clust, cluster_level, survival_file)
         os_data.append(np.array(os))    
     
-    axs_list[0].boxplot(gleason_data, patch_artist=True, medianprops = dict(color="blue",linewidth=2.3), boxprops=dict(facecolor='lightcyan'))
+    axs_list[0].boxplot(gleason_data, patch_artist=True, medianprops = dict(color="firebrick",linewidth=2.3), boxprops=dict(facecolor='mistyrose'))
     axs_list[0].set_ylabel('Gleason score')
     axs_list[0].set_xticks([y + 1 for y in range(len(gleason_data))], labels=['4.0', '4.1', '4.2', '4.3'])
     axs_list[0].set_xlabel('Cluster')
     axs_list[0].grid(True, axis='y')
     
     # Add OS time subplot.
-    axs_list[1].boxplot(os_data, patch_artist=True, medianprops=dict(color='blue', linewidth=2.3), boxprops=dict(facecolor='lightcyan'))
+    axs_list[1].boxplot(os_data, patch_artist=True, medianprops=dict(color='firebrick', linewidth=2.3), boxprops=dict(facecolor='mistyrose'))
     axs_list[1].set_ylabel('Survival time [days]')
     axs_list[1].set_xticks([y + 1 for y in range(len(os_data))], labels=['4.0', '4.1', '4.2', '4.3'])
     axs_list[1].set_xlabel('Cluster')
@@ -152,6 +152,198 @@ def plot_gleason_os_xcell(cluster_file_path : str, phenotype_file : str, surviva
     fig.tight_layout()
     plt.savefig("gleason_os_xcell.pdf", format='pdf')
     
+    
+def plot_fdrs():
+    """Plot FDR results from all used tools and input networks.
+    """
+    # Create one large mosaic plot.
+    fig, axs = plt.subplot_mosaic([['A', 'B'], ['C', 'D']])
+    fig.set_size_inches(12, 12)
+    axs_list = list(axs.values())
+    
+    # Axis label sizes.
+    axis_label_sizes = "large"
+    
+    ### Add PARADYS DysRegNet FDRS in top left plot.
+    file_path = '../results/fdrs/PARADYS_DysRegNet/fdrs_dysregnet_all_cohorts.csv'
+    df = pd.read_csv(file_path, sep=';')
+    df.rename(columns={"cohort" : "Cohort"}, inplace=True)
+    df.sort_values(by=['Cohort'], inplace=True, ascending=False)
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    sns.violinplot(x = df['decoy_rate'],
+                y = df['FDR'], 
+                hue = df['Cohort'],
+                cut=0,
+                ax=axs_list[0],
+                linewidth=1.4)
+    # axs.set_title("FDRs of PARADYS using DysRegNet networks")
+    axs_list[0].legend(prop={'size':18})
+    axs_list[0].set_ylabel("FDR")
+    axs_list[0].set_xlabel("Rate of decoy mutations")
+    axs_list[0].set_axisbelow(True)
+    axs_list[0].grid(axis='y')
+    
+    ### Add PRODIGY FDRS on bottom right plot.
+    file_path = '../results/fdrs/PRODIGY/fdrs_prodigy.csv'
+    df = pd.read_csv(file_path, sep=',')
+    rate_dict = {25 : '0.25', 5 : '0.50', 75 : '0.75'}
+    df.replace({"Rate": rate_dict}, inplace=True)
+    df.sort_values(by=['Cohort'], inplace=True, ascending=False)
+    
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    sns.violinplot(x = df['Rate'],
+                y = df['FDR'], 
+                hue = df['Cohort'],
+                ax=axs_list[3],
+                cut=0, linewidth=1.4)
+    # axs.set_title("FDRs of PARADYS using DysRegNet networks")
+    #axs_list[1].legend(loc='upper right', title='Cohort', title_fontsize='small')
+    axs_list[3].get_legend().remove()
+    axs_list[3].set_ylabel("FDR")
+    axs_list[3].set_xlabel("Rate of decoy mutations")
+    axs_list[3].set_axisbelow(True)
+    axs_list[3].grid(axis='y')
+    
+    ### Add PARADYS with SSN networks plots on bottom left.
+    cohorts = ['prad', 'coad']
+    file_path = '../results/fdrs/PARADYS_SSN/'
+    # Merge FDR files for different cohorts into one dataframe.
+    combined_df = pd.DataFrame(columns=['patient', 'FDR', 'Rate', 'Cohort'])
+    for cohort in cohorts:
+        file = file_path+f'fdrs_{cohort}.csv'
+        df = pd.read_csv(file)
+        df['Cohort']=cohort.upper()
+        df.drop(columns=['Unnamed: 0'], inplace=True)
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+        
+    combined_df.sort_values(by=['Cohort'], inplace=True, ascending=False)
+    
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    sns.violinplot(x = combined_df['Rate'],
+                y = combined_df['FDR'], 
+                hue = combined_df['Cohort'],
+                cut=0,
+                ax=axs_list[2],
+                linewidth=1.4)
+    axs_list[2].get_legend().remove()
+    axs_list[2].set_ylabel("FDR")
+    axs_list[2].set_xlabel("Rate of decoy mutations")
+    axs_list[2].set_axisbelow(True)
+    axs_list[2].grid(axis='y')
+    
+    ### Add PARADYS with DysRegNet_NoDir networks FDRS on top right.
+    cohorts = ['prad', 'coad']
+    file_path = '../results/fdrs/PARADYS_DysRegNet_NoDir/'
+    # Merge FDR files for different cohorts into one dataframe.
+    combined_df = pd.DataFrame(columns=['patient', 'FDR', 'Rate', 'Cohort'])
+    for cohort in cohorts:
+        file = file_path+f'fdrs_{cohort}.csv'
+        df = pd.read_csv(file)
+        df['Cohort']=cohort.upper()
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+        
+    combined_df.sort_values(by=['Cohort'], inplace=True, ascending=False)
+    
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    sns.violinplot(x = combined_df['Rate'],
+                y = combined_df['FDR'], 
+                hue = combined_df['Cohort'],
+                cut=0,
+                ax=axs_list[1],
+                linewidth=1.4)
+    axs_list[1].get_legend().remove()
+    axs_list[1].set_ylabel("FDR")
+    axs_list[1].set_xlabel("Rate of decoy mutations")
+    axs_list[1].set_axisbelow(True)
+    axs_list[1].grid(axis='y')
+    
+    # Add mosaic labels above plots.
+    for label, ax in axs.items():
+        # label physical distance to the left and up:
+        trans = mtransforms.ScaledTranslation(-20/72, 7/72, fig.dpi_scale_trans)
+        ax.text(0.0, 1.0, label, transform=ax.transAxes + trans,
+                fontsize='x-small', va='bottom', fontfamily='sans-serif', fontweight='bold')
+    
+    fig.tight_layout()
+    plt.savefig("fdrs.pdf", format='pdf')
+    
+
+def plot_fdrs_paradys():
+    """Plots violinplot of FDR of PARADYS colored by cohort and categorized by % simulated.
+    """
+    # Plot FDR files in multiple boxplots.
+    file_path = '../results/fdrs/PARADYS_DysRegNet/fdrs_dysregnet_all_cohorts.csv'
+    df = pd.read_csv(file_path, sep=';')
+    df.rename(columns={"cohort" : "Cohort"}, inplace=True)
+    #plt.ylim(0,0.04)
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    axs = sns.violinplot(x = df['decoy_rate'],
+                y = df['FDR'], 
+                hue = df['Cohort'],
+                cut=0)
+    # axs.set_title("FDRs of PARADYS using DysRegNet networks")
+    axs.set_ylabel("FDR")
+    axs.set_xlabel("Rate of decoy mutations")
+    plt.show()
+    
+def plot_fdrs_prodigy_new():
+    # Plot FDR files in multiple boxplots.
+    file_path = '../results/fdrs/PRODIGY/fdrs_prodigy.csv'
+    df = pd.read_csv(file_path, sep=',')
+    rate_dict = {25 : '0.25', 5 : '0.50', 75 : '0.75'}
+    df.replace({"Rate": rate_dict}, inplace=True)
+    
+    sns.set_theme(style='darkgrid')
+    sns.set(font_scale=3.0)
+    axs = sns.violinplot(x = df['Rate'],
+                y = df['FDR'], 
+                hue = df['Cohort'],
+                cut=0, linewidth=2.5)
+    # axs.set_title("FDRs of PARADYS using DysRegNet networks")
+    axs.legend(loc='upper right')
+    axs.set_ylabel("FDR")
+    axs.set_xlabel("Rate of decoy mutations")
+    plt.show()
+    
+    
+def plots_fdrs_prodigy():
+    """Combines PRODIGY FDR results and plots them together in violinplot.
+    """
+    cohorts = ['BRCA', 'COAD', 'PRAD']
+    rates = ['025', '05', '075']
+    rates_float = [0.25, 0.5, 0.75]
+    
+    df = pd.DataFrame(columns=["FDR", "Rate", "Cohort"])
+     
+    for type in cohorts:
+        for rate, rate_float in zip(rates, rates_float):
+            file_path = f'FPRs/False_mutations_PRODIGY/FDRs/{type}_{rate}_1_fdr_patients.csv'
+            sub_df = pd.read_csv(file_path, sep=',')
+            sub_df.drop(columns=['patient'], inplace=True)
+            sub_df['Rate'] = rate_float
+            sub_df['Cohort'] = type
+            df = pd.concat([df, sub_df], axis=0)
+    
+    print(df)
+    sns.set_style('whitegrid')
+    sns.set(font_scale=3.0)
+    axs = sns.violinplot(x = df['Rate'],
+                y = df['FDR'], 
+                hue = df['Cohort'],
+                cut=0)
+    # axs.set_title("FDRs of PARADYS using DysRegNet networks")
+    
+    axs.set_ylabel("FDR")
+    axs.set_xlabel("Rate of decoy mutations")
+    
+    plt.show()
+
+    
 
 if __name__=='__main__':
     
@@ -161,6 +353,9 @@ if __name__=='__main__':
     survival_file = "../data/PRAD_survival.txt"
     xcell_results_path = "../results/clustering/xcell/"
     plot_gleason_os_xcell(cluster_file_path, phenotype_file, survival_file, xcell_results_path)
+    
+    # Input paths for FDR plots.
+    #plot_fdrs()
     
     
 
